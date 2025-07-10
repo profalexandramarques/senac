37f1a8529cb3063a175203601df73ff69f6c9038
Alexandra Marques <prof.alexandra.marques@gmail.com>
2025-07-10 08:51:02 -0300

    Add files via upload

---
+++ python/variaveis.py
@@ -0,0 +1,10 @@
+print('Variáveis')
+nome = 'Alexandra'
+print('Bom dia '+nome)
+idade = 18
+print(idade)
+salario_minimo = 1450.99
+print(salario_minimo)
+ativo = True
+print(ativo)
+print(type(nome))
