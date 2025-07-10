61218f8ea97d9c8e5aaa8d4626bb9d30ab07a647
Alexandra Marques <prof.alexandra.marques@gmail.com>
2025-07-10 09:53:51 -0300

    Add files via upload

---
+++ python/idade.py
@@ -0,0 +1,5 @@
+print('=== Cálculo da Idade ===')
+ano_atual = 2025
+ano_nascimento = int(input('Digite o ano do nascimento: '))
+idade = ano_atual - ano_nascimento
+print('A sua idade é '+str(idade))
